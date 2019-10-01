0ff0e4718c67076cce2cd2119ba0ddec371b8e86
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-01 10:18:51 +0300

    fix deploy

--- src/express/api.ts
+++ src/express/api.ts
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 router.use(bodyParser.json());
 
-const posts = require('./db.json');
+const posts = require('../db.json');
 
 router.get('/api/posts', (req, res) => {
   res.send(posts);
